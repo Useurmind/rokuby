b2803159d9488c53c363ca2d95f6f0da32bf8fab
Jochen Gruen <gruen@intel-vci.uni-saarland.de>
2013-07-01 14:05:28 +0200

    Fixes to linux build.
    Signed-off-by: Jochen Gruen <gruen@intel-vci.uni-saarland.de>

--- lib/rokuby/Processors/Linux/gpp_project_compiler.rb
+++ lib/rokuby/Processors/Linux/gpp_project_compiler.rb
@@ -16,6 +16,15 @@ module Rokuby
       _RegisterInputTypes()
     end
 
+    # return a path that is compatible with the command line of linux
+    # @param [String] path The path to transform.
+    # @return [String] The path in a form that is compatile to the command line.
+    def _GetCommandLinePath(path)
+      rpath = path.gsub(/\s/, "\\\s")
+      #puts "Compatible path '#{rpath}'"
+      return rpath
+    end
+
     def _ProcessInputs(taskArgs=nil)
       platBinExt = GetPlatformBinaryExtensions(taskArgs)
       
@@ -62,8 +71,8 @@ module Rokuby
       #puts "Created compile command for #{sourcePath}: #{compileCommand}"
             
       return CreateFileTask({
-        filePath: targetPath.RelativePath,
-        dependencies: [sourcePath.RelativePath],
+        filePath: _GetCommandLinePath(targetPath.RelativePath),
+        dependencies: [_GetCommandLinePath(sourcePath.RelativePath)],
         command: compileCommand,
         error: "Could not compile #{sourcePath.to_s} to #{targetPath.to_s}."
       })
@@ -73,23 +82,25 @@ module Rokuby
       binaryPath = gppConf.OutputDirectory + ProjectPath.new(gppConf.TargetName + gppConf.TargetExt)
       
       commandParts = []
+
+      binaryCommandLinePath = _GetCommandLinePath(binaryPath.RelativePath)
       
       if(@projectDescription.BinaryType == :Application)
-        commandParts.concat(["g++", "-o", binaryPath.RelativePath])
+        commandParts.concat(["g++", "-o", binaryCommandLinePath])
         commandParts.concat(gppConf.LinkOptions)
         commandParts.concat(_MapCompileDefines(gppConf))
         commandParts.concat(compileTaskNames)
         commandParts.concat(_GatherLibraryLinkComponents(gppConf))
         
       elsif(@projectDescription.BinaryType == :Shared)
-        commandParts.concat(["g++", "-shared", "-fPIC", "-o", binaryPath.RelativePath])
+        commandParts.concat(["g++", "-shared", "-fPIC", "-o", binaryCommandLinePath])
         commandParts.concat(gppConf.LinkOptions)
         commandParts.concat(_MapCompileDefines(gppConf))
         commandParts.concat(compileTaskNames)
         commandParts.concat(_GatherLibraryLinkComponents(gppConf))
         
       elsif(@projectDescription.BinaryType == :Static)
-        commandParts.concat(["ar", "cq", binaryPath.RelativePath])
+        commandParts.concat(["ar", "cq", binaryCommandLinePath])
         commandParts.concat(compileTaskNames)
       else
         raise "Unknown BinaryType '#{@projectDescription.BinaryType}' encountered in #{self.class}"
@@ -97,7 +108,7 @@ module Rokuby
       command = commandParts.join(" ")
 
       return CreateFileTask({
-        filePath: binaryPath.RelativePath,
+        filePath: binaryCommandLinePath,
         dependencies: compileTaskNames,
         command: command,
         error: "Failed to link #{binaryPath}"
@@ -119,8 +130,8 @@ module Rokuby
       compileCommandParts.push(_MapCompileDefines(gppConf))
 
       compileCommandParts.push("-o")
-      compileCommandParts.push(targetPath.RelativePath)
-      compileCommandParts.push(sourcePath.RelativePath)
+      compileCommandParts.push(_GetCommandLinePath(targetPath.RelativePath))
+      compileCommandParts.push(_GetCommandLinePath(sourcePath.RelativePath))
       
       return compileCommandParts.join(" ")
     end
@@ -131,7 +142,7 @@ module Rokuby
         return includePaths
       end
       
-      @includePaths[gppConf.Platform.Name] = (gppConf.IncludePaths.map() {|include| Gpp::CommandLine::Options::INCLUDE_DIRECTORY + include.RelativePath()}).uniq
+      @includePaths[gppConf.Platform.Name] = (gppConf.IncludePaths.map() {|include| Gpp::CommandLine::Options::INCLUDE_DIRECTORY + _GetCommandLinePath(include.RelativePath())}).uniq
       return @includePaths[gppConf.Platform.Name]
     end
 
@@ -174,9 +185,9 @@ module Rokuby
         libExtension = libPath.FileExt()
         if(libExtension == dynamicLibExtension)
           dynamicLibs.push  Gpp::CommandLine::Options::LIB_NAME + _GetLibLinkName(libPath.FileName(false))
-          dynamicLibsSearchPaths.add Gpp::CommandLine::Options::LIB_DIRECTORY + libPath.DirectoryPath().RelativePath
+          dynamicLibsSearchPaths.add Gpp::CommandLine::Options::LIB_DIRECTORY + _GetCommandLinePath(libPath.DirectoryPath().RelativePath)
         else
-          staticLibs.push libPath.RelativePath
+          staticLibs.push _GetCommandLinePath(libPath.RelativePath)
         end
       end
       
@@ -186,9 +197,9 @@ module Rokuby
         projectLibFilePath = projGppConf.GetTargetFilePath().MakeRelativeTo(workingDir)
         if(projGppConf.TargetExt == Gpp::Configuration::TargetExt::SHARED_LIB)
           dynamicLibs.push Gpp::CommandLine::Options::LIB_NAME + _GetLibLinkName(projectLibFilePath.FileName(false))
-          dynamicLibsSearchPaths.add Gpp::CommandLine::Options::LIB_DIRECTORY + projectLibFilePath.DirectoryPath().RelativePath
+          dynamicLibsSearchPaths.add Gpp::CommandLine::Options::LIB_DIRECTORY + _GetCommandLinePath(projectLibFilePath.DirectoryPath().RelativePath)
         elsif(projGppConf.TargetExt == Gpp::Configuration::TargetExt::STATIC_LIB)
-          staticLibs.push projectLibFilePath.RelativePath
+          staticLibs.push _GetCommandLinePath(projectLibFilePath.RelativePath)
         end
       end
       
